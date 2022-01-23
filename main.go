f0467104dd1aa0a39e4d6ed7cdcd58e24d859e0c
smbirch <spencer.m.birch@gmail.com>
2022-01-22 18:58:07 -0800

    delete works now, but create post creates a Blog with empty fields

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -23,17 +24,20 @@ func Welcome(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Welcome to the blog!\n\n")
 	io.WriteString(w, "'/posts' - See all posts\n")
 	io.WriteString(w, "'/posts/{id}' - Find a post by its ID\n")
-	io.WriteString(w, "'/posts/{id}' Can also be used to delete a post!")
+	io.WriteString(w, "'/posts/{id}' Can also be used to delete a post!\n")
 
 	fmt.Println("hit: '/'")
 }
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	json.NewEncoder(w).Encode(Posts)
 	fmt.Println("hit: /posts")
 }
 
 func GetPostByID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 
@@ -42,32 +46,46 @@ func GetPostByID(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(post)
 		}
 	}
-	fmt.Println("hit: '/posts/{id}'")
+	fmt.Println("hit: '/posts/{id}' - by ID")
 }
 
+//this is also broken because
 func CreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var post Blog
+	json.Unmarshal(reqBody, &post)
+
+	Posts = append(Posts, post)
+
+	json.NewEncoder(w).Encode(Posts)
+
+	fmt.Println("hit: '/posts/create'")
 
 }
 
-//This...doesnt seem to work, likely because of how my "database" is "storing" the json
+//This is broken
 func DeletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 
 	for i, post := range Posts {
 		if post.Id == id {
 			Posts = append(Posts[:i], Posts[i+1:]...)
+
 		}
 	}
+	fmt.Println("hit: '/posts/{id}/delete'")
 }
 
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", Welcome)
-	myRouter.HandleFunc("/posts", GetAllPosts)
-	myRouter.HandleFunc("/posts/{id}", GetPostByID)
-	myRouter.HandleFunc("/posts", CreatePost).Methods("POST")
-	myRouter.HandleFunc("/posts/{id}", DeletePost).Methods("DELETE")
+	myRouter.HandleFunc("/posts", GetAllPosts).Methods("GET")
+	myRouter.HandleFunc("/post", CreatePost).Methods("POST")
+	myRouter.HandleFunc("/post/{id}", GetPostByID).Methods("GET")
+	myRouter.HandleFunc("/post/{id}", DeletePost).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", myRouter))
 }
@@ -86,6 +104,24 @@ func main() {
 			Title:   "PostTwo",
 			Author:  "Spencer",
 			Content: "This is the content of the second post",
+		},
+		{
+			Id:      "3",
+			Title:   "PostThree",
+			Author:  "Briana",
+			Content: "Somethin about a drumkit, idk",
+		},
+		{
+			Id:      "4",
+			Title:   "PostFour",
+			Author:  "Briana",
+			Content: "Nutty Chocolate Cherry Trail Mix",
+		},
+		{
+			Id:      "5",
+			Title:   "PostFive",
+			Author:  "Briana",
+			Content: "Succotash does not belong in gumbo!",
 		},
 	}
 	handleRequests()
